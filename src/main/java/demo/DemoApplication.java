0eee61f127bb4f85937c660a499041d8cf0e2570
glyashen <glyashen@razormind.co.uk>
2016-08-03 13:32:20 +0300

    Add static index page

--- src/main/java/demo/DemoApplication.java
+++ src/main/java/demo/DemoApplication.java
@@ -5,14 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
 @SpringBootApplication
 public class DemoApplication {
 
-    @RequestMapping(value = {"", "/"})
-    public String index() {
-        return "Running ...";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
