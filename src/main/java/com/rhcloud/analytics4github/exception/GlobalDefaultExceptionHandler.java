3f2b47131e13cc78b0e270e49368f3def042b09a
nazar <khimin1719@gmail.com>
2017-03-03 09:01:16 +0200

    Added scheduled task for trendingpage cashe

--- src/main/java/com/rhcloud/analytics4github/exception/GlobalDefaultExceptionHandler.java
+++ src/main/java/com/rhcloud/analytics4github/exception/GlobalDefaultExceptionHandler.java
@@ -6,20 +6,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * Created by Nazar on 28.12.2016.
+ * exception handling  apply across the whole application, not just to an individual controller.
  */
 @ControllerAdvice
-public class GlobalDefaultExceptionHandler  {
+public class GlobalDefaultExceptionHandler extends ResponseEntityExceptionHandler {
     private static Logger LOG = getLogger(GlobalDefaultExceptionHandler.class);
 
+
     @ExceptionHandler(value = {GitHubRESTApiException.class})
-    public ResponseEntity<String> gitHubRESTAPIException(GitHubRESTApiException exception) throws GitHubRESTApiException {
+    public ResponseEntity gitHubRESTAPIException(GitHubRESTApiException exception) {
         LOG.error(exception.getMessage(), exception);
-        return new ResponseEntity<String>(exception.getMessage(), HttpStatus.NOT_FOUND);
+        return  ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(exception.getMessage());
     }
 
 }
